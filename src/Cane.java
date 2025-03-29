e5bf0e2ce6dbfe67552719fe2d8fb365058315ab
leonardo-fortin <leonardo.fortin@itiszuccante.edu.it>
2025-03-29 23:04:56 +0100

    Creazioni classi principali
    Classe TesterCanile e Canile

---
+++ src/Cane.java
@@ -0,0 +1,27 @@
+public class Cane {
+
+    private String nome;
+    private String razza;
+
+    public Cane(String nome, String razza){
+        this.nome = nome;
+        this.razza = razza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazza() {
+        return razza;
+    }
+
+    public void setRazza(String razza) {
+        this.razza = razza;
+    }
+
+}
---
+++ src/TesterCanile.java
@@ -0,0 +1,7 @@
+public class TesterCanile {
+
+    public static void main(String[] args) {
+
+    }
+
+}
