b41593aabca362f1adf95ae343d9c3583b1f7bd0
leonardo-fortin <leonardo.fortin@itiszuccante.edu.it>
2025-03-29 23:13:25 +0100

    Creazione metodi essenziali su TesterCanile
    Metodi stampaDescrizione(), inizializzaVariabili() e popola()

--- src/TesterCanile.java
+++ src/TesterCanile.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+
 public class TesterCanile {
 
-    public static void main(String[] args) {
+    private static ArrayList<Cane> cani;
 
+    private static void inizializzaVariabili(){
+        cani = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        stampaDescrizione();
+        inizializzaVariabili();
+        popola();
+    }
+
+    public static void stampaDescrizione(){
+        System.out.println("Canile");
+    }
+
+    public static void popola(){
+        cani.add(new Cane("Oscar", "Labrador"));
+        cani.add(new Cane("Neve", "Maltese"));
+        cani.add(new Cane("Lucky", "Border Collie"));
     }
 
 }
